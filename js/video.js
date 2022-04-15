f46439346d78acb6dffbcfc3f762090852e9045d
Joyce Kim <74065115+jkizzle1@users.noreply.github.com>
2022-04-14 21:07:54 -0400

    Update video.js

--- js/video.js
+++ js/video.js
@@ -5,14 +5,15 @@ window.addEventListener("load", function() {
 
 });
 
-/*Play Button: Play the video and update the volume information. */
+/* Play Button: Play the video and update the volume information.
+NOT FINISHED */ 
 document.querySelector("#play").addEventListener("click", function() {
 	console.log("Play Video");
 	video.play();
 
 });
 
-/*Pause Button: Pause the video.*/
+/* Pause Button: Pause the video.*/
 
 document.querySelector("#pause").addEventListener
 ("click", function() {
@@ -20,8 +21,42 @@ document.querySelector("#pause").addEventListener
 	video.pause();
 });
 
-/*Slow Down: Slow the current video speed by 5% each time 
+/* Slow Down: Slow the current video speed by 5% each time 
 the button is clicked and log the new speed to the console.  */
+
+document.querySelector("#slower").addEventListener
+("click", function() {
+	video.playbackRate *= 0.95;
+	console.log("The new speed is" + video.playbackRate);
+});
+
+/* Speed Up: Increase the current video speed each time the button is clicked 
+and log the new speed to the console.  Change it by an amount 
+proportional to the slow down. CHECK THIS!!  If you slow down 
+three times and then speed up three times you should be within 
+5 digits of 100% again.*/
+
+
+
+/* Skip Ahead: Advance the current video by 15 seconds.  If the video length 
+has been exceeded go back to the start of the video - no farther. 
+Log the current location of the video.*/
+
+
+
+/* Mute: Mute/unmute the video and update the text in the button.*/
+
+
+/* Volume Slider:Change the volume based on the slider and update the volume information.*/
+
+
+
+/* Styled: Utilize the existing oldSchool class on the video element*/
+
+
+
+
+/*Original: Remove the oldSchool class from the video.*/
 
 
 
