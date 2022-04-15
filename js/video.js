1cc5ec8c19b8ed99827e515d231bff45a38b8c6e
Joyce Kim <74065115+jkizzle1@users.noreply.github.com>
2022-04-14 20:53:26 -0400

    var video

--- js/video.js
+++ js/video.js
@@ -1,11 +1,21 @@
-var video;
+var video = document.querySelector(".video");
 
 window.addEventListener("load", function() {
 	console.log("Good job opening the window")
 
 });
 
-// document.querySelector("#play").addEventListener("click", function() {
-// 	console.log("Play Video");
-// });
+document.querySelector("#play").addEventListener("click", function() {
+	console.log("Play Video");
+	video.play();
+
+});
+
+/*Pause Button: Pause the video.*/
+
+document.querySelector("#pause").addEventListener
+("click", function() {
+	console.log("Pause Video");
+	video.pause();
+});
 
